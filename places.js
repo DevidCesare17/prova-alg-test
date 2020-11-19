d6e666a6831523b73fad9d80f1da9312f86ecd7c
Davide Cesare <davidereaper@gmail.com>
2020-11-19 18:19:25 +0100

    test

---
+++ places.js
@@ -0,0 +1,6 @@
+//SOLO INPUT BOX
+// var placesAutocomplete = places({
+//   appId: 'plMMPUODJ2PK',
+//   apiKey: '0337776886165fd5d9d75984ac961fa9',
+//   container: document.querySelector('#address-input')
+// });
